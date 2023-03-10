b37f6df7e74d69f72c643761f4cd877d5ed31a72
Ghimirevivek <stephengmre@gmail.com>
2023-03-10 22:52:28 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import '../styles/App.css';
+import '../styles/App.css'
 const App = () => {
-
-  const handleInput = (event) =>{
-   // use console.log
+  const handleInput = (event) => {
+    // use console.log
+    const val = event.target.value
+    console.log(`Input in #text-input is ${val}`)
   }
 
   // do not change id of input elements
   return (
     <div id="main">
-      <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <label htmlFor="text-input">Text Input:- </label>
+      <input id="text-input" type={'text'} onChange={handleInput} />
 
-      <br/>
-      <br/>
+      <br />
+      <br />
 
-      <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
-      <br/>
+      <label htmlFor="num-input">Number input</label>
+      <input id="num-input" type={'number'} onChange={handleInput} />
+      <br />
     </div>
   )
 }
 
-
-export default App;
+export default App
